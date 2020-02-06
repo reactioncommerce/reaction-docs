bd8fd7ca12c996f4e31f7dbfc3ef4a5ca571e358
Erik Kieckhafer <ek@ato.la>
2020-02-06 10:33:19 -0800

    Update versions.js
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- website/pages/en/versions.js
+++ website/pages/en/versions.js
@@ -51,7 +51,7 @@ class Versions extends React.Component {
             <table className="versions">
               <tbody>
                 <tr>
-                  <th>3.0.0-next</th>
+                  <th>v3-next</th>
                   <td>
                     <a href={`${baseURL}docs/next/intro.html`}>Documentation</a>
                   </td>
